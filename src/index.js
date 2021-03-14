cadeab0836450302f75f54a72c79536a436de71a
gpereira62 <gustavopereirasantos@hotmail.com>
2021-03-14 17:17:47 -0300

    Create Middleware - verifyIfExistsAccountCPF

--- src/index.js
+++ src/index.js
@@ -2,17 +2,24 @@ const express = require('express');
 const { v4: uuidv4 } = require("uuid");
 
 const app = express();
-
 app.use(express.json());
 
 const customers = [];
 
-/**
- * cpf - string
- * name - string
- * id - uuid
- * statment []
- */
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer){
+    return response.status(400).json ({ error: "Customer not found" });
+  }
+
+  request.customer = customer;
+
+  return next();
+}
 
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
@@ -36,15 +43,8 @@ app.post("/account", (request, response) => {
 
 });
 
-app.get("/statement/:cpf", (request, response) => {
-  const { cpf } = request.params; 
-
-  const customer = customers.find((customer) => customer.cpf === cpf);
-
-  if (!customer){
-    return response.status(400).json ({ error: "Customer not found" });
-  }
-
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
   return response.json(customer.statement);
 });
 
